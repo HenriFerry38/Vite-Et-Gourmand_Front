730c1c4a3bc4cd0d8c7bc2550bd0b285de2df59c
Henri FERRY <henricosmique@gmail.com>
2026-01-15 17:32:23 +0100

    personalisation de la route menu, css modifié

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html")
+    new Route("/menus", "Nos Menus", "/pages/menu.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
