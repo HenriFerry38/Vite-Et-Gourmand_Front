d94b1d12246bf86235e787d3116bba2d6e1b67a4
Henri FERRY <henricosmique@gmail.com>
2026-01-16 17:00:14 +0100

    Création des routes et pages cgv et mentions légale, Modifications de la page Home, Scss Bootstrap

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/menus", "Nos Menus", "/pages/menu.html")
+    new Route("/menus", "Nos Menus", "/pages/menu.html"),
+    new Route("/cgv", "Conditions Générales de Vente", "/pages/cgv.html"),
+    new Route("/mentions_legales", "Mentions Légales", "/pages/mentions_legales.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
