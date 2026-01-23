786655397ea4f123c4bef08776136069c41abefb
Henri FERRY <henricosmique@gmail.com>
2026-01-23 13:42:21 +0100

    Création route account, Signup et route editPassword. Création signup.html, account.html et editPassword.html, scss en fonction.

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,11 @@ export const allRoutes = [
     new Route("/menus", "Nos Menus", "/pages/menus.html"),
     new Route("/cgv", "Conditions Générales de Vente", "/pages/cgv.html"),
     new Route("/mentions_legales", "Mentions Légales", "/pages/mentions_legales.html"),
-    new Route("/signin", "Inscription", "/pages/signin.html")
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword","Modifier son mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/historiqueCommande", "Historique des commandes", "/pages/auth/historiqueCommande.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
